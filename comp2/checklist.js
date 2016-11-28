55719fb09c0945dcf5ad34c3e0f39550681979da
Talvinder Singh <talvindersingh1992@gmail.com>
2016-11-28 13:18:16 +0530

    Update checklist.js

--- comp2/checklist.js
+++ comp2/checklist.js
@@ -1,4 +1,4 @@
-
+<script type="text/javascript">
 $(document).ready(function(){  
 
 function checkList(ddId){
@@ -49,4 +49,4 @@ $('input[name="selectedVal"]:checked').each(function() {
 });
 
 });
-
+</script>
